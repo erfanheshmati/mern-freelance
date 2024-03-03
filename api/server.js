a0d393dfac2d7aa5bd87da47ac8d264ba32f5552
Erfan Heshmati <heshmati74@gmail.com>
2024-03-03 10:03:14 +0330

    connect to mongodb

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, (req, res) => {
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.listen(5000, () => {
+  connect();
   console.log("Server is running on port 5000");
 });
