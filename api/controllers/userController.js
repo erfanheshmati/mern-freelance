9a329bec98b4c77610915a4b799e12ec36e00ce9
Erfan Heshmati <heshmati74@gmail.com>
2024-03-03 16:39:36 +0330

    delete user api

---
+++ api/controllers/userController.js
@@ -0,0 +1,12 @@
+import User from "../models/userModel.js";
+import createError from "../utils/createError.js";
+
+export const deleteUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return next(createError(403, "You can only delete your account"));
+  }
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("User deleted");
+};
