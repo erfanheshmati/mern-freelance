0328af10abaf1435780dd3f582b4edba46fd8872
Erfan Heshmati <heshmati74@gmail.com>
2024-03-04 11:49:56 +0330

    search gig api

--- api/controllers/gigController.js
+++ api/controllers/gigController.js
@@ -42,8 +42,17 @@ export const getGig = async (req, res, next) => {
 };
 
 export const getGigs = async (req, res, next) => {
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
+    }),
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+  };
   try {
-    const gigs = await Gig.find();
+    const gigs = await Gig.find(filters);
     res.status(200).json(gigs);
   } catch (err) {
     next(err);
